9ff44de7210535a38e824d1eab3b3f0a754f6364
tamrivera05 <tamangelarivera@gmail.com>
2024-08-04 09:39:30 +0800

    Feat: errors.sol

---
+++ errors.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+contract OwnershipContract {
+    address public owner;
+    bool public isActive;
+
+    constructor() {
+        owner = msg.sender;
+        isActive = false;
+    }
+
+    modifier onlyOwner {
+        require(msg.sender == owner, "Only the owner can access.");
+        _;
+    }
+
+    function activate() public onlyOwner {
+        isActive = true;
+    }
+
+    function deactivate() public onlyOwner {
+        isActive = false;
+    }
+
+    function checkStatus() public view {
+        if (isActive == false) {
+            revert("The contract is not active.");
+        } else {
+            revert("The contract is active.");
+        }
+    }
+
+    function verifyOwner() public view {
+        assert(msg.sender == owner);
+    }
+}
