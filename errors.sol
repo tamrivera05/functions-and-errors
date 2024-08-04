89b416704db816b42e04a7847d52d0c0eca0a113
tamrivera05 <tamangelarivera@gmail.com>
2024-08-04 09:17:34 +0800

    Update contract

--- errors.sol
+++ errors.sol
@@ -1,55 +1,38 @@
 // SPDX-License-Identifier: MIT
-pragma solidity  0.8.26;
+pragma solidity ^0.8.17;
 
-contract MyToken {
+contract OwnershipContract {
+    address public owner;
+    bool public isActive;
 
     constructor() {
         owner = msg.sender;
+        isActive = false;
     }
 
-
-    string public tokenName = "TOKEN";
-    string public tokenAbbrv = "TKN";
-    uint256 public totalSupply = 0;
-    address public owner;
-
-    event Mint(address indexed to, uint amount);
-    event Burn(address indexed from, uint amount);
-    event Transfer(address indexed from, address indexed to, uint256 amount);
-
-    error insufficientBalance(uint256 balance, uint withdrawAmount);
-
-    mapping(address => uint256) public balances;
-
-    modifier ownerOnly {
-        assert(msg.sender == owner);
+    modifier onlyOwner {
+        require(msg.sender == owner, "Only the owner can access.");
         _;
     }
 
+    function activate() public onlyOwner {
+        isActive = true;
+    }
 
-    function burn (address _address, uint256 _value) public ownerOnly{
-        if (balances[_address] < _value){
-            revert insufficientBalance({balance: balances[_address], withdrawAmount: _value});
-        }else{
-            totalSupply -= _value;
-            balances[_address] -= _value;
-            emit Burn(_address, _value);
+    function deactivate() public onlyOwner {
+        isActive = false;
+    }
+
+    function checkStatus() public view {
+        if (isActive == false) {
+            revert("The contract is not active.");
+        } else {
+            revert("The contract is active.");
         }
     }
 
-    function mint (address _address, uint256 _value) public ownerOnly{
-        totalSupply += _value;
-        balances[_address] += _value;
-        emit Mint(_address, _value);
+    function verifyOwner() public view {
+        assert(msg.sender == owner);
     }
-
-    function transfer (address _receiver, uint _value) public{
-        require(balances[msg.sender] >= _value, "Account balance must be greater than transfered value");
-        balances[msg.sender] -= _value;
-        balances[_receiver] += _value;
-        emit Transfer(msg.sender, _receiver, _value);
-    }
-
-
-
 }
+
